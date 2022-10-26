8164929dd9c0efe7d8cccd358452a7fa8f295d3a
MattHenao <70972324+MattHenao@users.noreply.github.com>
2022-10-26 10:26:48 -0500

    First commit Pokemon API

---
+++ pokemon/src/main/java/com/pokemon/model/Pokemon.java
@@ -0,0 +1,73 @@
+package com.pokemon.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="pokemon")
+
+
+public class Pokemon {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private Integer idPokedex;
+	private String name;
+	private String type;
+	private Integer attack;
+	
+	
+	public Pokemon() {
+	}
+
+	public Pokemon(Integer id, Integer idPokedex, String name, String type, Integer attack) {
+
+		this.id = id;
+		this.idPokedex = idPokedex;
+		this.name = name;
+		this.type = type;
+		this.attack = attack;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getIdPokedex() {
+		return idPokedex;
+	}
+	public void setIdPokedex(Integer idPokedex) {
+		this.idPokedex = idPokedex;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public Integer getAttack() {
+		return attack;
+	}
+	public void setAttack(Integer attack) {
+		this.attack = attack;
+	}
+	@Override
+	public String toString() {
+		return "Pokemon [id=" + id + ", idPokedex=" + idPokedex + ", name=" + name + ", type=" + type + ", attack="
+				+ attack + "]";
+	}
+	
+	
+} 
